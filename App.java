994e51e8927682bc328a7322fdad0af51f02d2b5
Luiziki <89998654+Luiziki@users.noreply.github.com>
2023-07-29 22:57:20 -0300

    App, A e B

---
+++ App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        A teste = new A();
+        
+        teste.MA1();
+        teste.MA2();
+        teste.MA3();
+
+        B testeB = new B();
+
+        testeB.MB1();
+        testeB.MB2();
+
+        C testeC = new C();
+
+        testeC.MC1();
+        testeC.MC2();
+
+    };
+}
