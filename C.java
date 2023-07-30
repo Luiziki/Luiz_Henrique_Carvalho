0d272cd5c6dae0c18718cb670248d588ea16a720
Luiz Henrique Reis de Carvalho <luiz.carvalho4@estudante.ufla.br>
2023-07-29 23:33:29 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,35 @@
+public class C {
+
+        // Atributo
+        private int c1;
+        private float c2;
+        
+        // Método construtor
+        public C() {
+        }
+
+        public int getC1() {
+            return c1;
+        }
+
+        public float getC2() {
+            return c2;
+        }
+
+        public void setC1(int b1) {
+            this.c1 = b1;
+        }
+
+        public void setC2(float b2) {
+            this.c2 = b2;
+        }
+
+        // Metodo
+        public void MC1() {
+            System.out.println("Método MC1.");
+        }
+
+        public void MC2() {
+            System.out.println("Método MC2.");
+        }
+    };
